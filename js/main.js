90357e5689fb10867d278827bdf9de45a12e19b2
jonathan joly <41478399+joly534@users.noreply.github.com>
2020-04-19 11:40:35 +0200

    update

--- js/main.js
+++ js/main.js
@@ -26,16 +26,17 @@ function preload () {
 }
 
 function create () {
+
     player = this.physics.add.sprite(100,100, 'hero');
     player.setCollideWorldBounds(true);
 
     this.anims.create({
         key: 'turn',
         frames: [ { key: 'hero', frame: 19 } ],
-        frameRate: 30
+        frameRate: 20
     });
 
-    //le joueur se déplace à gauche
+    //animation du joueur se déplaçant à gauche
     this.anims.create({
         key:'left',
         frames: this.anims.generateFrameNumbers('hero',{start: 10,end: 17}),
@@ -43,7 +44,7 @@ function create () {
         repeat: -1
     });
 
-    //le joueur se déplace à droite
+    //animation du joueur se déplaçant à droite
     this.anims.create({
         key: 'right',
         frames: this.anims.generateFrameNumbers('hero', { start: 28, end: 37 }),
@@ -51,7 +52,7 @@ function create () {
         repeat: -1
     });
 
-    //le joueur se déplace vers le haut
+    //animation du joueur se déplaçant vers le haut
     this.anims.create({
         key: 'up',
         frames: this.anims.generateFrameNumbers('hero', { start: 1, end: 8 }),
@@ -59,7 +60,7 @@ function create () {
         repeat: -1
     });
 
-    //le joueur se déplace vers le bas
+    //animation du joueur se déplaçant vers le bas
     this.anims.create({
         key: 'down',
         frames: this.anims.generateFrameNumbers('hero', { start: 19, end: 26 }),
@@ -74,29 +75,30 @@ function update () {
 
     // action lorsque la fleche de gauche est enfoncée
     if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-    player.anims.play('left', true);
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
 
-// action lorsque la fleche de droite est enfoncée
-} else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-    player.anims.play('right', true);
+    // action lorsque la fleche de droite est enfoncée
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
 
-// action lorsque la fleche du haut est enfoncée
-} else if (cursors.up.isDown) {
-    player.setVelocityY(-160);
-    player.anims.play('up', true);
+    // action lorsque la fleche du haut est enfoncée
+    } else if (cursors.up.isDown) {
+        player.setVelocityY(-160);
+        player.anims.play('up', true);
 
-// action lorsque la fleche du bas est enfoncée
-} else if (cursors.down.isDown) {
-    player.setVelocityY(160);
-    player.anims.play('down', true);
+    // action lorsque la fleche du bas est enfoncée
+    } else if (cursors.down.isDown) {
+        player.setVelocityY(160);
+        player.anims.play('down', true);
 
-} else {
-    player.setVelocityX(0);
-    player.anims.play('turn');
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
 
-} if (cursors.up.isDown && player.body.touching.down){
-    player.setVelocityY(-330);
-}
+    } if (cursors.up.isDown && player.body.touching.down){
+        player.setVelocityY(-330);
+    }
+    
 }
